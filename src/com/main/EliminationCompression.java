ea92c2047c7b4aebfd8b0b27580e9fbe1c129e4a
teguhsatyadharma <hiteguhsatya@gmail.com>
2022-07-18 21:00:38 +0800

    adding binary search

--- src/com/main/EliminationCompression.java
+++ src/com/main/EliminationCompression.java
@@ -1,7 +1,6 @@
 package com.main;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class EliminationCompression {
     private int[] arrays;
@@ -11,39 +10,62 @@ public class EliminationCompression {
     }
 
     public void compress() {
-        int[] array = quickSort(0, 7);
-        System.out.println(Arrays.toString(this.arrays));
+        int[] arr = this.arrays.clone();
+        arr = quickSort(0, 7, arr);
+        // System.out.println("Unsorted:" + Arrays.toString(this.arrays));
+        System.out.println("Sorted:" + Arrays.toString(arr));
+        /*
+         * Compression:
+         * Select i item from array
+         * Return its index in sorted array
+         * Convert to binaries and determine its length
+         */
+        for (int i = 0; i < this.arrays.length; i++) {
+            int idx = binarySearch(this.arrays[i], arr, 0, 7);
+            System.out.println(idx);
+        }
 
-        // Sort using quick sort
-        // Begin compression
     }
 
-    public int[] quickSort(int low, int high) {
+    public int binarySearch(int num, int[] array, int low, int high) {
+        if (low > high) {
+            return -1;
+        }
+        int mid = low + (high - low) / 2;
+        if (num == array[mid]) {
+            return mid;
+        } else if (num > array[mid]) {
+            return binarySearch(num, array, mid + 1, high);
+        } else {
+            return binarySearch(num, array, low, mid - 1);
+        }
+    }
+
+    public int[] quickSort(int low, int high, int[] arr) {
         // code courtesy of geeksforgeeks
         if (low < high) {
-            int pivotIndex = getPartition(low, high);
-            quickSort(low, pivotIndex - 1);
-            quickSort(pivotIndex + 1, high);
+            int pivotIndex = getPartition(low, high, arr);
+            quickSort(low, pivotIndex - 1, arr);
+            quickSort(pivotIndex + 1, high, arr);
         }
-        return this.arrays;
+        return arr;
     }
 
-    public int getPartition(int low, int high) {
+    public int getPartition(int low, int high, int[] arr) {
         int temp = 0;
-        int pivot = this.arrays[high];
+        int pivot = arr[high];
         int i = (low - 1);
         for (int j = low; j <= high - 1; j++) {
-            if (this.arrays[j] < pivot) {
+            if (arr[j] < pivot) {
                 i++;
-                temp = this.arrays[i];
-                this.arrays[i] = this.arrays[j];
-                this.arrays[j] = temp;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
-        temp = this.arrays[i + 1];
-        this.arrays[i + 1] = this.arrays[high];
-        this.arrays[high] = temp;
-        // System.out.println(Arrays.toString(this.arrays));
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
         return (i + 1);
     }
 }
